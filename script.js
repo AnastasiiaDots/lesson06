b929f9246a25c9ae25f0db71b7313d06052bb0f1
Anastasiia Dotsenko <anastasiia_dots@yahoo.com>
2023-02-07 11:31:31 +1300

    lesson06 modified

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 'use strict'
 
-let guessfunc = function () {
-    function Res() {
-        let randomNum = Math.floor(Math.random() * 100) + 1;
+function guessfunc(randomNum) {
+    function res() {
+        randomNum = Math.floor(Math.random() * 100) + 1;
 
         let userNum = prompt('Угадай число от 1 до 100', 'Введите число');
 
         if (isNaN(userNum)) {
             alert('Введи число!');
+
         } if (userNum === null) {
             alert('Игра окончена');
 
@@ -16,14 +17,15 @@ let guessfunc = function () {
         } else if (userNum < randomNum) {
             alert('Загаданное число меньше');
             userNum = + prompt('Введите число');
+            res();
         } else if (userNum > randomNum) {
             alert('Загаданное число больше');
             userNum = +prompt('Введите число');
+            res();
         }
     }
-    return Res;
+    res()
 }
 
 let guessNumber = guessfunc();
-
 guessNumber();
