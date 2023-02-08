cc16c314d9552fd8b86d328d818d0acb2bdf3302
Anastasiia Dotsenko <anastasiia_dots@yahoo.com>
2023-02-08 14:21:09 +1300

    lesson06 final modification

--- script.js
+++ script.js
@@ -1,50 +1,39 @@
 'use strict'
 
 function guessfunc() {
-    function res() {
 
-        const randomNum = Math.floor(Math.random() * 100) + 1;
+    const randomNum = Math.floor(Math.random() * 100) + 1;
+    function res() {
 
         let userNum = + prompt('Угадай число от 1 до 100');
 
-        // if (isNaN(userNum)) {
-        //     alert('Введи число!');
-
-        // } else if (userNum === null) {
-        //     alert('Игра окончена');
-
-        // } else if (userNum == randomNum) {
-        //     alert('Поздравляю, Вы угадали!!!');
-
-        // } else if (userNum < randomNum) {
-        //     alert('Загаданное число больше');
-        //     res();
-        // } else if (userNum > randomNum) {
-        //     alert('Загаданное число меньше');
-        //     res();
-        // }
         switch (true) {
-            case userNum < randomNum:
-                alert('Загаданное число меньше');
+
+            case !isNaN(userNum === null):
+                alert('Игра окончена');
+                break;
+
+            case isNaN(userNum):
+                alert('Введи число!');
                 res();
                 break;
             case userNum > randomNum:
+                alert('Загаданное число меньше');
+                res();
+                break;
+            case userNum < randomNum:
                 alert('Загаданное число больше');
                 res();
                 break;
+
             case userNum == randomNum:
                 alert('Поздравляю, Вы угадали!!!');
-                break;
-            case isNaN(userNum):
-                alert('Введи число!');
-                break;
-            case userNum === null:
-                alert('Игра окончена');
+
+
         }
 
     }
     res()
 }
 
-let guessNumber = guessfunc();
-guessNumber();
+guessfunc()
